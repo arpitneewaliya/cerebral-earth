1d2b3f2a5711f82e3b9a12c0ef1378a204845c90
Arpit Neewaliya <arpit.neewaliya@gmail.com>
2026-06-25 10:52:06 +0530

    add vercel analytics to the project

--- news-app/src/index.jsx
+++ news-app/src/index.jsx
@@ -4,6 +4,7 @@ import '@fontsource/inter';
 import './index.css';
 import App from './App.jsx';
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,8 @@ root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 );
+
